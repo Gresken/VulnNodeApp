f7b5615cc1a1b284db015ea13daeadbb0051b9aa
Nicolai Gresken <gresken@outlook.com>
2023-08-16 18:41:55 -0300

    variables

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 var config = {
-    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"127.0.0.1",
-    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"database",
-    DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"user",
-    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"pass",
+    DATABASE_HOST: process.env.DATABASE_HOST?process.env.DATABASE_HOST:"challenge-db",
+    DATABASE_NAME: process.env.DATABASE_NAME?process.env.DATABASE_NAME:"vuln_node_app_db",
+    DATABASE_USER: process.env.DATABASE_USER?process.env.DATABASE_USER:"vulnnodeapp",
+    DATABASE_PASS: process.env.DATABASE_PASS?process.env.DATABASE_PASS:"password",
 }
 
 module.exports = config;
